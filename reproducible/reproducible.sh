7cf5adb4ad560c74a213e4841139f072ae348ad8
Hadi Amjad <46374292+hadiamjad@users.noreply.github.com>
2025-01-28 20:02:24 +0500

    Delete reproducible directory

--- reproducible/reproducible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 -W ignore table2.py
-python3 -W ignore table3.py
-python3 -W ignore table4.py
-python3 -W ignore fig4.py
-python3 -W ignore fig5.py
-python3 -W ignore fig8a.py
-python3 -W ignore fig8b.py
-python3 -W ignore fig8c.py
