aac77247e3355dad853f15884f8c8c3c0d7b11fa
Radhofan <radhofanazizi@gmail.com>
2025-03-26 11:37:53 +0700

    ada

--- src/reproducible/reproducible.sh
+++ src/reproducible/reproducible.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python3 -W ignore table2.py
-python3 -W ignore table3.py
-python3 -W ignore table4.py
-python3 -W ignore fig4.py
-python3 -W ignore fig5.py
-python3 -W ignore fig8a.py
-python3 -W ignore fig8b.py
-python3 -W ignore fig8c.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/table2.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/table3.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/table4.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/fig4.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/fig5.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/fig8a.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/fig8b.py
+python3 -W ignore Web-Ads-Accessibility/src/reproducible/fig8c.py
